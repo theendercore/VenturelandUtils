6fa329e83467053bd1f4d3c6ca40764692bdcd07
TheEnderCore <theendercore@gmail.com>
2025-06-28 00:24:23 +0300

    1.0.0 prep

--- src/client/kotlin/com/theendercore/ventureland_utils/config/TemplateConfig.kt
+++ src/client/kotlin/com/theendercore/ventureland_utils/config/TemplateConfig.kt
@@ -1,18 +1,18 @@
 package com.theendercore.ventureland_utils.config
-
-import com.theendercore.ventureland_utils.VenturelandUtilsClient.MODID
-import com.theendercore.ventureland_utils.VenturelandUtilsClient.id
-import me.fzzyhmstrs.fzzy_config.annotations.NonSync
-import me.fzzyhmstrs.fzzy_config.config.Config
-import me.fzzyhmstrs.fzzy_config.config.ConfigGroup
-import me.fzzyhmstrs.fzzy_config.validation.number.ValidatedInt
-import me.fzzyhmstrs.fzzy_config.validation.number.ValidatedNumber.WidgetType.TEXTBOX_WITH_BUTTONS
-
-@Suppress("unused")
-class TemplateConfig : Config(id(MODID)) {
-    var groupName = ConfigGroup("group_id", false)
-    var commonEntry = ValidatedInt(0, 10, -10, TEXTBOX_WITH_BUTTONS)
-    @NonSync
-    @ConfigGroup.Pop
-    var clientEntry = true
-}
+//
+//import com.theendercore.ventureland_utils.VenturelandUtilsClient.MODID
+//import com.theendercore.ventureland_utils.VenturelandUtilsClient.id
+//import me.fzzyhmstrs.fzzy_config.annotations.NonSync
+//import me.fzzyhmstrs.fzzy_config.config.Config
+//import me.fzzyhmstrs.fzzy_config.config.ConfigGroup
+//import me.fzzyhmstrs.fzzy_config.validation.number.ValidatedInt
+//import me.fzzyhmstrs.fzzy_config.validation.number.ValidatedNumber.WidgetType.TEXTBOX_WITH_BUTTONS
+//
+//@Suppress("unused")
+//class TemplateConfig : Config(id(MODID)) {
+//    var groupName = ConfigGroup("group_id", false)
+//    var commonEntry = ValidatedInt(0, 10, -10, TEXTBOX_WITH_BUTTONS)
+//    @NonSync
+//    @ConfigGroup.Pop
+//    var clientEntry = true
+//}
