21a9a05ecaef9c305fc43fccc5645a18439d6981
TheEnderCore <theendercore@gmail.com>
2025-12-11 14:30:15 +0200

    invisible entity hitbox blacklist

--- src/client/java/com/theendercore/ventureland_utils/mixin/EntityRenderDispatcherMixin.java
+++ src/client/java/com/theendercore/ventureland_utils/mixin/EntityRenderDispatcherMixin.java
@@ -14,7 +14,7 @@ import static com.theendercore.ventureland_utils.VenturelandUtilsClient.config;
 public class EntityRenderDispatcherMixin<E extends Entity> {
     @ModifyExpressionValue(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/Entity;isInvisible()Z", ordinal = 1))
     boolean showHitboxesForInv(boolean original, @Local(argsOnly = true) E entity) {
-        if ((entity instanceof PlayerEntity)) return original;
+        if ((entity instanceof PlayerEntity) || config.getHitboxBlacklist().contains(entity.getType())) return original;
         return !config.getInvisibleEntityHitboxes();
     }
 }
--- src/client/kotlin/com/theendercore/ventureland_utils/config/VenturelandUtilsConfig.kt
+++ src/client/kotlin/com/theendercore/ventureland_utils/config/VenturelandUtilsConfig.kt
@@ -4,9 +4,16 @@ import com.theendercore.ventureland_utils.VenturelandUtilsClient.MODID
 import com.theendercore.ventureland_utils.VenturelandUtilsClient.id
 import me.fzzyhmstrs.fzzy_config.annotations.Comment
 import me.fzzyhmstrs.fzzy_config.config.Config
+import me.fzzyhmstrs.fzzy_config.validation.collection.ValidatedList
+import me.fzzyhmstrs.fzzy_config.validation.minecraft.ValidatedRegistryType
+import net.minecraft.entity.EntityType
+import net.minecraft.registry.Registries
 
 @Suppress("unused")
 class VenturelandUtilsConfig : Config(id(MODID)) {
-    @Comment("Shows hitboxes for invisible mobs.")
+    @Comment("Shows hitboxes for invisible mobs")
     var invisibleEntityHitboxes = true
+    @Comment("List of entity types that will not have there hitboxes when invisible")
+    var hitboxBlacklist =
+        ValidatedList(listOf(EntityType.ARMOR_STAND), ValidatedRegistryType.of(Registries.ENTITY_TYPE))
 }
