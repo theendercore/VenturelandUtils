b06f8602df7837bfd9b594a4542b3fd0a080d19a
TheEnderCore <theendercore@gmail.com>
2026-01-07 16:32:18 +0200

    config

--- src/client/kotlin/com/theendercore/ventureland_utils/config/VenturelandUtilsConfig.kt
+++ src/client/kotlin/com/theendercore/ventureland_utils/config/VenturelandUtilsConfig.kt
@@ -9,6 +9,7 @@ import me.fzzyhmstrs.fzzy_config.validation.collection.ValidatedList
 import me.fzzyhmstrs.fzzy_config.validation.minecraft.ValidatedRegistryType
 import me.fzzyhmstrs.fzzy_config.validation.misc.ValidatedColor
 import me.fzzyhmstrs.fzzy_config.validation.number.ValidatedInt
+import me.fzzyhmstrs.fzzy_config.validation.number.ValidatedNumber
 import net.minecraft.entity.EntityType
 import net.minecraft.registry.Registries
 import java.awt.Color
@@ -27,18 +28,17 @@ class VenturelandUtilsConfig : Config(id(MODID)) {
     @Comment("Hides the hitboxes for items that are used to custom display texts like HP")
     var hideDisplayItemHitboxes = true
 
+    @Comment("Cooldown in seconds")
     var cooldownGroup = ConfigGroup("cooldowns")
 
-    @Comment("Cooldown in seconds")
-    var scrollOfLifeCooldown = ValidatedInt(5 * 60, 1024, 1)
+    var scrollOfLifeCooldown = ValidatedInt(5 * 60, 1024, 1, ValidatedNumber.WidgetType.TEXTBOX_WITH_BUTTONS)
     var scrollOfLifeColor = ValidatedColor(255, 85, 255)
 
     var revengeColor = ValidatedColor(170, 0, 0)
 
-    var resurrectionCooldown = ValidatedInt(100, 1024, 1)
+    var resurrectionCooldown = ValidatedInt(100, 1024, 1, ValidatedNumber.WidgetType.TEXTBOX_WITH_BUTTONS)
 
     @ConfigGroup.Pop
-    @Comment("Cooldown in seconds")
     var resurrectionColor = ValidatedColor(255, 85, 85)
 
     @Comment("Used to print debug info. Don't turn it on unless asked to do so")
